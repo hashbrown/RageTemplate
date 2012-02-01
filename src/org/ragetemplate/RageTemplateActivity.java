a1b1e16fa4c8a1859fdc4b72de49458c2d07d1bf
Phil Olson <phil@code42.com>
2012-01-31 18:41:31 -0600

    First commit

---
+++ src/org/ragetemplate/RageTemplateActivity.java
@@ -0,0 +1,13 @@
+package org.ragetemplate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RageTemplateActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
